7c383cb8fcacba8efe12c5639e368ceced05a4ea
David Peterson <peterdav000@warren.k12.in.us>
2024-08-07 13:40:32 -0400

    0000

---
+++ src/assignment.java
@@ -0,0 +1,9 @@
+public class assignment {
+    public static void main(String[] args) {
+
+        double pounds = 200;
+        double kilograms = 0.45359237 + pounds;
+        System.out.println("kilograms =" + kilograms );
+    }
+}
+
